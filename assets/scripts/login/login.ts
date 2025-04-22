441404a65ca08ce0e762cb0063d1b4dcad269cf6
kakamatong <631603920@qq.com>
2025-04-22 14:53:26 +0800

    代码优化

--- assets/scripts/login/login.ts
+++ assets/scripts/login/login.ts
@@ -7,14 +7,20 @@ import { LOGIN_INFO } from '../datacenter/interfaceConfig';
 import { DataCenter } from '../datacenter/datacenter';
 const { ccclass, property } = _decorator;
 
+export interface ACCOUNT_INFO {
+    username: string;
+    password: string;
+    server?: string;
+    loginType?: string;
+}
 @ccclass('Login')
 export class Login implements handleSocketMessage {
-    private socket: Socket | null = null;
-    private loginMsg:string = '';
-    private stepid = 0
-    private clientPrivateKey: CryptoJS.lib.WordArray;
-    private challenge: CryptoJS.lib.WordArray;
-    private loginInfo:LOGIN_INFO = {
+    private _socket: Socket | null = null;
+    private _loginMsg:string = '';
+    private _stepid = 0
+    private _clientPrivateKey: CryptoJS.lib.WordArray;
+    private _challenge: CryptoJS.lib.WordArray;
+    private _loginInfo:LOGIN_INFO = {
         username:'',
         userid:0,
         password:'',
@@ -23,25 +29,33 @@ export class Login implements handleSocketMessage {
         token:'',
         subid:0
     };
-    private callBack:(b:boolean)=>void = (b:boolean)=>{};
+    private _callBack:(b:boolean)=>void = (b:boolean)=>{};
+    private _accountInfo: ACCOUNT_INFO | null = null;
 
-    start(func:(b:boolean)=>void) {
-        this.callBack = func;
+
+    start(acc: ACCOUNT_INFO,func:(b:boolean)=>void) {
+        this._accountInfo = acc;
+        this._callBack = func;
         console.log('login');
         this.encode_token();
         this.initSocket();
     }
 
-
+    // test001
+    // wlj123456
     encode_token(){
-        const strUser = 'test001';
-        const strPassword = 'wlj123456';
+        if(!this._accountInfo){
+            this._callBack && this._callBack(false);
+            return;
+        }
+        const strUser = this._accountInfo.username;
+        const strPassword = this._accountInfo.password;
         const strServer = 'lobbyGate';
         const strLogintype = 'account';
-        this.loginInfo.username = strUser;
-        this.loginInfo.password = strPassword;
-        this.loginInfo.server = strServer;
-        this.loginInfo.loginType = strLogintype;
+        this._loginInfo.username = strUser;
+        this._loginInfo.password = strPassword;
+        this._loginInfo.server = strServer;
+        this._loginInfo.loginType = strLogintype;
         const user = CryptoJS.enc.Utf8.parse(strUser).toString(CryptoJS.enc.Base64);
         const password = CryptoJS.enc.Utf8.parse(strPassword).toString(CryptoJS.enc.Base64);
         const server = CryptoJS.enc.Utf8.parse(strServer).toString(CryptoJS.enc.Base64);
@@ -49,17 +63,17 @@ export class Login implements handleSocketMessage {
         const token = user + '@' + server + ':' + password + '#' + logtinType;
         
         console.log('token:', token);
-        this.loginMsg = token;
+        this._loginMsg = token;
     }
 
     initSocket() {
-        this.socket = new Socket();
-        this.socket.init('ws://192.168.1.140:8002');
-        this.socket.setHandleMessage(this);
+        this._socket = new Socket();
+        this._socket.init('ws://192.168.1.140:8002');
+        this._socket.setHandleMessage(this);
     }
 
     sendMessage(message: any) {
-        this.socket && this.socket.sendMessage(message);
+        this._socket && this._socket.sendMessage(message);
     }
 
     onOpen(event: any) {
@@ -78,22 +92,22 @@ export class Login implements handleSocketMessage {
             const msg2 = atob(infos[2]);
             if(code === '200'){
                 log('登录成功');
-                this.loginInfo.subid = Number(msg);
-                this.loginInfo.userid = Number(msg2);
-                DataCenter.instance.setLoginInfo(this.loginInfo);
-                this.callBack(true);
+                this._loginInfo.subid = Number(msg);
+                this._loginInfo.userid = Number(msg2);
+                DataCenter.instance.setLoginInfo(this._loginInfo);
+                this._callBack(true);
             }else{
                 log('登录失败code:', code);
-                this.callBack(false);
+                this._callBack(false);
             }
             return;
         }
-        if(this.stepid === 0) {
+        if(this._stepid === 0) {
             this.performAuthentication1(text)
-            this.stepid = 1;
-        }else if(this.stepid === 1) {
+            this._stepid = 1;
+        }else if(this._stepid === 1) {
             this.performAuthentication2(text)
-            this.stepid = 2;
+            this._stepid = 2;
         }
     }
 
@@ -110,13 +124,13 @@ export class Login implements handleSocketMessage {
         // 1. 接收服务端发送的challenge（伪代码示例）
         const challengeB64 = message; // 接收base64字符串
         console.log('challengeB64:', challengeB64);
-        this.challenge = CryptoJS.enc.Base64.parse(challengeB64.trim());
+        this._challenge = CryptoJS.enc.Base64.parse(challengeB64.trim());
     
         // 生成客户端密钥对前添加日志
-        this.clientPrivateKey = CryptoJS.lib.WordArray.random(8);
-        //this.clientPrivateKey = CryptoJS.lib.WordArray.create(new Uint8Array([0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]));
+        this._clientPrivateKey = CryptoJS.lib.WordArray.random(8);
+        //this._clientPrivateKey = CryptoJS.lib.WordArray.create(new Uint8Array([0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]));
 
-        const clientPrivateKeyDh = dhexchange(this.clientPrivateKey);
+        const clientPrivateKeyDh = dhexchange(this._clientPrivateKey);
 
         const clientPublicKeyB64 = CryptoJS.enc.Base64.stringify(clientPrivateKeyDh);
         const messageReq = clientPublicKeyB64
@@ -135,15 +149,15 @@ export class Login implements handleSocketMessage {
         const serverPublicKey = CryptoJS.enc.Base64.parse(serverPublicKeyB64.trim());
     
         // 4. 计算共享密钥（根据服务端实现调整）
-        const secret = dhsecret(serverPublicKey, this.clientPrivateKey);
+        const secret = dhsecret(serverPublicKey, this._clientPrivateKey);
         //const secret = CryptoJS.lib.WordArray.create(new Uint8Array([0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]));
 
         // 打印secret
         const secretHex = secret.toString(CryptoJS.enc.Hex);
         console.log('secretHex:', secretHex);
-        this.loginInfo.token = secretHex;
+        this._loginInfo.token = secretHex;
         // 5. 计算HMAC校验
-        const hmac = hmac64(this.challenge, secret);
+        const hmac = hmac64(this._challenge, secret);
         //hmac.sigBytes = 8; // 截取前8字节
         const hmacB64 = CryptoJS.enc.Base64.stringify(hmac);
         console.log('hmacB64:', hmacB64);
@@ -154,7 +168,7 @@ export class Login implements handleSocketMessage {
         this.sendMessage(hmacB64Array);
     
         //secret = CryptoJS.lib.WordArray.create(new Uint8Array([0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08]));
-        const encryptedToken = customDESEncrypt(this.loginMsg, secret);
+        const encryptedToken = customDESEncrypt(this._loginMsg, secret);
         this.sendMessage(encryptedToken);
     }
 
