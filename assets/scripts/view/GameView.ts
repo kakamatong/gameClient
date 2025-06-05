d96e21300eaac1317bead09bd20824ff51b76db4
kakamatong <631603920@qq.com>
2025-06-05 14:12:15 +0800

    对局玩家信息

--- assets/scripts/view/GameView.ts
+++ assets/scripts/view/GameView.ts
@@ -5,7 +5,9 @@ import { LogColors } from '../frameworks/framework';
 import { GAME_PLAYER_INFO } from '../datacenter/interfaceConfig';
 import { DataCenter } from '../datacenter/datacenter';
 
+import * as fgui from "fairygui-cc";
 const { ccclass, property } = _decorator;
+const SELF_LOCAL = 1;
 @ccclass('GameView')
 export class GameView extends FGUIGameView {
     private _maxPlayer = 2;
@@ -41,23 +43,38 @@ export class GameView extends FGUIGameView {
     }
 
     getSelfSeat(): number {
-        return this._playerList[1].seat;
+        return this._playerList[SELF_LOCAL].seat;
+    }
+
+    showPlayerInfo(seat:number):void{
+        const playerInfo = this._playerList[seat];
+        if(playerInfo){
+            if(seat == SELF_LOCAL){
+                this.UI_TXT_NICKNAME_1.text = playerInfo.nickname;
+                this.UI_TXT_USERID_1.text = playerInfo.userid.toString();
+            }else if (seat == 2){
+                this.UI_TXT_NICKNAME_2.text = playerInfo.nickname;
+                this.UI_TXT_USERID_2.text = playerInfo.userid.toString();
+            }
+        }
     }
 
     onReportGamePlayerInfo(data: any): void {
         console.log(LogColors.green(data.msg));
         const selfid = DataCenter.instance.userid;
         if(data.userid == selfid){
-            this._playerList[1] = data;
+            this._playerList[SELF_LOCAL] = data;
+            this.showPlayerInfo(SELF_LOCAL);
         }else{
             const local = this.seat2local(data.seat);
             this._playerList[local] = data;
+            this.showPlayerInfo(local);
         }
     }
 
     seat2local(seat: number): number {
         const selfSeat = this.getSelfSeat();
-        const selfLocal = 1
+        const selfLocal = SELF_LOCAL
         const d = (seat - selfSeat) % this._maxPlayer;
         if(d > 0){
             return selfLocal + d;
