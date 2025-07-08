d0cac73bb3dd68e3e9a94b3bdaa6b07eea01b588
kakamatong <631603920@qq.com>
2025-07-08 11:29:48 +0800

    feat(网络): 添加游戏Socket管理器单例实现
    实现游戏Socket管理器的单例模式，用于统一管理游戏中的Socket连接

---
+++ assets/scripts/frameworks/gameSocketManager.ts
@@ -0,0 +1,16 @@
+import { SocketManager } from './socketManager';
+
+export class GameSocketManager extends SocketManager {
+    //单例
+    private static _instance: GameSocketManager;
+    public static get instance(): GameSocketManager {
+        if (!this._instance) {
+            this._instance = new GameSocketManager();
+        }
+        return this._instance;
+    }
+
+    constructor(){
+        super();
+    }
+}
