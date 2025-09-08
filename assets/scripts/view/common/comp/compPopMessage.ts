d84efbb75ec0ee6127771670df88a8ef818334cd
kakamatong <631603920@qq.com>
2025-09-08 17:12:21 +0800

    fix(comp): 修复弹窗消息组件回调调用错误
    - 将 show 方法参数改为可选以增强兼容性
    - 修正回调函数调用时错误使用 this.data 的问题，改为正确使用 this._data
    - 保证按钮关闭、取消和确认事件均调用正确的回调函数属性
    - 修正代码格式以提升代码可读性和一致性

--- assets/scripts/view/common/comp/compPopMessage.ts
+++ assets/scripts/view/common/comp/compPopMessage.ts
@@ -5,22 +5,22 @@ import * as fgui from "fairygui-cc";
 
 export class CompMessage extends FGUICompPopMessage { 
     private _data:any | null = null;
-    show(data:any){
+    show(data?:any){
         this._data = data;
         this.UI_TXT_TITLE.text = data.title ?? "";
         this.UI_TXT_CONTENT.text = data.content ?? "";
     }
 
     onBtnClose(): void {
-        this._data && (this.data.closeBack && this.data.closeBack())
+        this._data && (this._data.closeBack && this._data.closeBack())
     }
 
     onBtnCancel(): void {
-        this._data && (this.data.closeBack && this.data.cancelBack())
+        this._data && (this._data.closeBack && this._data.cancelBack())
     }
 
     onBtnSure(): void {
-        this._data && (this.data.closeBack && this.data.sureBack())
+        this._data && (this._data.closeBack && this._data.sureBack())
     }
 }
 fgui.UIObjectFactory.setExtension(CompMessage.URL, CompMessage);
