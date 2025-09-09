aa420c973cfc84bfe4c3c6da4dae0d3462a4bcc6
kakamatong <631603920@qq.com>
2025-09-09 13:17:08 +0800

    refactor(rank): 重构Rank类，拆分请求用户与全部排名接口
    - 移除Rank单例实现，简化类结构
    - 将req方法拆分为reqAll和reqSelf分别请求全部和用户排名
    - 对应响应方法resp拆分为respAll和respSelf
    - 调整回调调用和日志打印逻辑，提高代码可读性

--- assets/scripts/modules/rank.ts
+++ assets/scripts/modules/rank.ts
@@ -2,32 +2,14 @@ import { LobbySocketManager } from '../frameworks/lobbySocketManager';
 import { LogColors } from '../frameworks/framework';
 
 export class Rank {
-    /**
-     * @property {Rank} _instance - 单例实例
-     * @private
-     * @static
-     */
-    private static _instance: Rank;
+
     private _callBack:((b:boolean, data:any)=>void) | null = null; // 登入成功，但不一定已经拉到数据
-    /**
-     * @method instance
-     * @description 获取Match的单例实例
-     * @static
-     * @returns {Rank} Match单例实例
-     */
-    public static get instance(): Rank {
-        if (!this._instance) {
-            this._instance = new Rank();
-        }
-        return this._instance;
-    }
 
-
-    req(callBack?:(b:boolean,data:any)=>void) {
+    reqAll(callBack?:(b:boolean,data:any)=>void) {
         if (callBack) {
             this._callBack = callBack;
         }
-        LobbySocketManager.instance.sendToServer('callActivityFunc',{moduleName : 'gameRank', funcName : 'getRankList', args:JSON.stringify({})} , this.resp.bind(this))
+        LobbySocketManager.instance.sendToServer('callActivityFunc',{moduleName : 'gameRank', funcName : 'getRankList', args:JSON.stringify({})} , this.respAll.bind(this))
     }
 
     /**
@@ -35,7 +17,7 @@ export class Rank {
      * @description 处理返回
      * @param {any} result - 服务器返回的匹配结果
      */
-    resp(result: any) {
+    respAll(result: any) {
         if(result && result.code == 1){
             const res = JSON.parse(result.result);
             if(res.error){
@@ -46,6 +28,26 @@ export class Rank {
             }
         }else{
             this._callBack && this._callBack(false, null)
+        }
+    }
+
+    reqSelf(callBack?:(b:boolean,data:any)=>void) {
+        if (callBack) {
+            this._callBack = callBack;
+        }
+        LobbySocketManager.instance.sendToServer('callActivityFunc',{moduleName : 'gameRank', funcName : 'getRank', args:JSON.stringify({})}, this.respSelf.bind(this))
+    }
+
+    respSelf(result: any) {
+        if(result && result.code == 1){
+            const res = JSON.parse(result.result);
+            if(res.error){
+                console.log(LogColors.red(res.error));
+                this._callBack && this._callBack(false, res)
+            }else{
+                this._callBack && this._callBack(true, res)
+            }
+        }else{
         }
     }
 }
