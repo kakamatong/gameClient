9f80a74f6da1c5a1596c4f028edd9a913a210ec8
kakamatong <631603920@qq.com>
2025-09-16 20:32:32 +0800

    feat(mail): 优化邮件列表显示与交互逻辑
    - 绑定邮件数据列表，并实现条目渲染器
    - 消息标题和过期时间动态显示
    - 点击邮件条目触发处理函数
    - 根据邮件状态调整已读/未读显示
    - 移除部分文本组件的多余属性以简化UI控件配置
    - 根据数据是否存在切换邮件列表显示状态

--- assets/scripts/view/mail/mailView.ts
+++ assets/scripts/view/mail/mailView.ts
@@ -2,10 +2,35 @@ import FGUIMatchView from '../../fgui/mail/FGUIMailView';
 import * as fgui from "fairygui-cc";
 
 export class MailView extends FGUIMatchView {
-
+    private _list:any[] = [];
+            
     show(data?: any):void{
+        if(data && data.length > 0){
+            this._list = data
+            this.UI_LV_LIST.itemRenderer = this.itemRenderer.bind(this)
+            this.UI_LV_LIST.numItems = data.length
+            this.ctrl_have.selectedIndex = 1;
+        }else{
+            this.ctrl_have.selectedIndex = 0;
+        }
+    }
+
+    itemRenderer(index:number, item:fgui.GObject){
+        const itemData = this._list[index];
+        item.asCom.getChild('UI_TXT_TITLE').text = itemData.title;
+        item.asCom.getChild('UI_TXT_TIME').text = `过期：${itemData.endTime}`;
+        item.asCom.onClick(()=>{
+            this.onBtnTitle(item, index);
+        })
+        const tmp = itemData.status > 0 ? 1 : 0;
+
+        item.asCom.getController('ctrl_read').selectedIndex = tmp;
+    }
+
+    onBtnTitle(item:fgui.GObject, index:number): void { 
         
     }
+    
 
     onBtnClose(): void {
         MailView.hideView();
