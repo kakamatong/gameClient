8b3f3ee150429baec18f03345f52e0b29df50ef5
kakamatong <631603920@qq.com>
2025-09-17 20:27:55 +0800

    feat(privateRoom): 添加私人房间加入组件和视图的UI绑定代码
    - 更新CompPrivateJoin.xml中按钮和文本组件的name属性，增强识别性
    - 修改package.xml以支持代码生成
    - 新增FGUICompPrivateJoin.ts，自动生成加入界面组件绑定及事件处理函数定义
    - 新增FGUIPrivateRoomView.ts，实现私人房间主视图的UI绑定及加载逻辑
    - 实现各按钮点击事件的空方法，预留后续逻辑扩展接口
    - 统一界面组件实例管理，确保视图只显示一份实例

---
+++ assets/scripts/fgui/privateRoom/FGUIPrivateRoomView.ts
@@ -0,0 +1,54 @@
+/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
+
+import { assetManager, AssetManager } from "cc";
+import * as fgui from "fairygui-cc";
+
+export default class FGUIPrivateRoomView extends fgui.GComponent {
+
+	public ctrl_private:fgui.Controller;
+	public static URL:string = "ui://s0qy2rl1nomu0";
+
+	public static packageName:string = "privateRoom";
+
+	public static instance:any | null = null;
+
+	public static showView(params?:any, callBack?:(b:boolean)=>void):void {
+		if(FGUIPrivateRoomView.instance) {
+			console.log("allready show");
+			callBack&&callBack(false);
+			return;
+		}
+		const bundle = assetManager.getBundle("fgui") as AssetManager.Bundle;
+		fgui.UIPackage.loadPackage(bundle, this.packageName, (error, pkg)=> {
+
+			if(error){console.log("loadPackage error", error);callBack&&callBack(false);return;}
+			const view = fgui.UIPackage.createObject("privateRoom", "PrivateRoomView") as FGUIPrivateRoomView;
+
+			view.makeFullScreen();
+			FGUIPrivateRoomView.instance = view;
+			fgui.GRoot.inst.addChild(view);
+			view.show && view.show(params);
+			callBack&&callBack(true);
+		}
+		);
+	}
+
+	protected onDestroy():void {
+		super.onDestroy();
+		FGUIPrivateRoomView.instance = null;
+	}
+	public static hideView():void {
+		FGUIPrivateRoomView.instance && FGUIPrivateRoomView.instance.dispose();
+	}
+
+	show(data?:any):void{};
+
+	public static createInstance():FGUIPrivateRoomView {
+		return <FGUIPrivateRoomView>(fgui.UIPackage.createObject("privateRoom", "PrivateRoomView"));
+	}
+
+	protected onConstruct():void {
+		this.ctrl_private = this.getControllerAt(0);
+	}
+}
+fgui.UIObjectFactory.setExtension(FGUIPrivateRoomView.URL, FGUIPrivateRoomView);
