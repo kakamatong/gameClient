462779f84994300a8440b0dae25569ffa109e769
kakamatong <631603920@qq.com>
2025-09-25 20:55:07 +0800

    feat(user-center): 添加用户中心视图及游戏记录展示功能
    - 创建 UserCenterView 类，继承 FGUIUserCenterView
    - 实现 show 方法，异步请求并展示用户游戏胜负记录
    - 计算并显示用户胜率，保留两位小数
    - 实现 updateUserInfo 方法，更新用户昵称、ID 和头像
    - 注册 UserCenterView 到 fgui.UIObjectFactory 用于界面扩展

---
+++ assets/scripts/view/userCenter/userCenterView.ts
@@ -0,0 +1,28 @@
+import FGUIUserCenterView from '../../fgui/userCenter/FGUIUserCenterView';
+import * as fgui from "fairygui-cc";
+import { UserGameRecord } from '../../modules/userGameRecord';
+import FGUICompHead from '../../fgui/common/FGUICompHead';
+import { DataCenter } from '../../datacenter/datacenter';
+
+export class UserCenterView extends FGUIUserCenterView {
+    show(data?: any):void{
+        const gameRecords = new UserGameRecord()
+        const func = (data:any)=>{
+            this.UI_TXT_WIN.text = data.win;
+            this.UI_TXT_LOSE.text = data.lose;
+            this.UI_TXT_DRAW.text = data.draw;
+            const rate = data.win / (data.win + data.lose + data.draw) * 100;
+            this.UI_TXT_RATE.text = `${rate.toFixed(2)}%`;
+        }
+        gameRecords.req(func)
+
+        this.updateUserInfo()
+    }
+
+    updateUserInfo():void{
+        this.UI_TXT_NICKNAME.text = DataCenter.instance.userData?.nickname ?? ''
+        this.UI_TXT_USERID.text = `${DataCenter.instance.userid ?? 0}`;
+        (this.UI_COMP_HEAD as FGUICompHead).UI_LOADER_HEAD.url = DataCenter.instance.headurl
+    }
+}
+fgui.UIObjectFactory.setExtension(UserCenterView.URL, UserCenterView);
