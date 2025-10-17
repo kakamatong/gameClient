183f00eae109d2a8b9f5330cb03509c2b89b1aad
kakamatong <631603920@qq.com>
2025-10-17 17:18:30 +0800

    feat(result): 在结果界面中添加胜负音效播放功能
    - 根据游戏结果标志播放胜利或失败音效
    - 通过 assetManager 加载音效资源并播放
    - 添加播放胜利音效的 playWinSound 方法
    - 添加播放失败音效的 playLoseSound 方法
    - 新增胜利和失败音效文件及其资源元数据

--- assets/scripts/games/game10001/view/result/ResultView.ts
+++ assets/scripts/games/game10001/view/result/ResultView.ts
@@ -1,12 +1,19 @@
 import FGUIResultView from '../../../../fgui/game10001Result/FGUIResultView';
 import * as fgui from "fairygui-cc";
 import { GameData } from '../../data/Gamedata';
+import { assetManager, AudioClip, log } from 'cc';
 
 export class ResultView extends FGUIResultView { 
     private _continueFunc:(()=>void) | null = null;
     private _scoreData:Array<{userid:number, cpData:any, nickname:string}> = []
     show(data?:any){
-        this.ctrl_flag.selectedIndex = data?.flag ?? 0;
+        const flag = data?.flag ?? 0
+        this.ctrl_flag.selectedIndex = flag;
+        if (flag ===1) {
+            this.playWinSound()
+        }else if (flag === 0) {
+            this.playLoseSound()
+        }
         this.ctrl_roomType.selectedIndex = GameData.instance.isPrivateRoom ? 1 : 0
         
         this._continueFunc = data?.continueFunc
@@ -19,6 +26,40 @@ export class ResultView extends FGUIResultView {
             this.UI_LV_GAME_INFO.itemRenderer = this.itemRenderer.bind(this)
             this.UI_LV_GAME_INFO.numItems = data.scores.length
         }
+    }
+
+    playWinSound(){
+        assetManager.loadBundle('sound', (err, bundle) => { 
+            if (err) {
+                log('loadBundle error', err);
+                return;
+            }
+
+            bundle.load<AudioClip>('game10001/win', (err, asset: AudioClip) => { 
+                if (err) {
+                    log('loadBundle error', err);
+                    return;
+                }
+                fgui.GRoot.inst.playOneShotSound(asset)
+            })
+        });
+    }
+
+    playLoseSound(){
+        assetManager.loadBundle('sound', (err, bundle) => { 
+            if (err) {
+                log('loadBundle error', err);
+                return;
+            }
+
+            bundle.load<AudioClip>('game10001/lose', (err, asset: AudioClip) => { 
+                if (err) {
+                    log('loadBundle error', err);
+                    return;
+                }
+                fgui.GRoot.inst.playOneShotSound(asset)
+            })
+        });
     }
 
     itemRenderer(index:number, item:fgui.GObject){
