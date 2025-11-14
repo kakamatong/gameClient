5bb296d32bb33bcc291d8a7fec1b8689463c132f
kakamatong <631603920@qq.com>
2025-11-14 11:53:17 +0800

    feat(game10001): 增强玩家头像组件交互和UI更新
    - 初始化时调用onChangedPos以更新UI文本位置
    - 监听位置控制(ctrl_pos)变化以动态调整文本锚点和对齐方式
    - 监听本地座位控制(ctrl_localSeat)变化同步localSeat属性
    - 为玩家头像(UI_COMP_HEAD)添加点击事件处理器onHeadClick
    - 点击事件中根据localSeat获取玩家数据并显示玩家信息界面
    - 增强了玩家头像组件的交互逻辑和界面响应能力

--- assets/scripts/games/game10001/view/game/comp/CompPlayerHead.ts
+++ assets/scripts/games/game10001/view/game/comp/CompPlayerHead.ts
@@ -8,11 +8,18 @@ export class CompPlayerHead extends FGUICompPlayerHead {
     public localSeat:number = 0;
     protected onConstruct(){
         super.onConstruct();
-        this.localSeat = this.ctrl_localSeat.selectedIndex;
         this.initUI()
     }
 
     initUI(){
+        this.onChangedPos()
+        this.ctrl_pos.onChanged(this.onChangedPos, this)
+        this.ctrl_localSeat.onChanged(this.onChangedLocalSeat, this)
+        this.UI_COMP_HEAD.onClick(this.onHeadClick, this)
+        this.localSeat = this.ctrl_localSeat.selectedIndex
+    }
+
+    onChangedPos(): void {
         if (this.ctrl_pos.selectedIndex == 0) {
             this.UI_TXT_ID.pivotX = 0
             this.UI_TXT_ID.pivotY = 0
@@ -28,10 +35,14 @@ export class CompPlayerHead extends FGUICompPlayerHead {
             this.UI_TXT_NICKNAME.pivotY = 0
             this.UI_TXT_NICKNAME.align = HorizontalTextAlignment.RIGHT
         }
-        this.UI_COMP_HEAD.onClick(this.onHeadClick, this)
+    }
+
+    onChangedLocalSeat(): void { 
+        this.localSeat = this.ctrl_localSeat.selectedIndex
     }
 
     onHeadClick(): void {
+        console.log("onHeadClick:", this.ctrl_pos.selectedIndex)
         const player = GameData.instance.getPlayerByLocal(this.localSeat)
         if (player) {
             PlayerInfoView.showView({userid:player.userid, cp: player.cp})
