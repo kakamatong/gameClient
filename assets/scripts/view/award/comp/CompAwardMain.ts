c35f0b277aca244b9c0f00180370fdd6a8891c8c
kakamatong <631603920@qq.com>
2026-01-20 20:51:28 +0800

    feat(view): 添加显示和列表渲染功能
    - 在CompAwardMain中新增show方法用于显示奖项数据
    - 添加initUI方法初始化界面元素和奖项列表
    - 实现itemRenderer方法用于奖项列表的渲染
    - 在ComProp组件中添加show方法支持根据传入道具信息显示数量
    - 增强组件的交互和数据展示能力

--- assets/scripts/view/award/comp/CompAwardMain.ts
+++ assets/scripts/view/award/comp/CompAwardMain.ts
@@ -8,6 +8,10 @@ import { AwardNotices } from "../../../modules/AwardNotices";
 @ViewClass()
 export class CompAwardMain extends FGUICompAwardMain { 
     private _data:AwardConfig | null = null;
+    /**
+     * 显示
+     * @param args 
+     */
     show(args:AwardConfig):void{
         this._data = args;
         
@@ -19,11 +23,19 @@ export class CompAwardMain extends FGUICompAwardMain {
         this.initUI();
     }
 
+    /**
+     * 初始化UI
+     */
     initUI():void{
         this.UI_LIST_AWARD.itemRenderer = this.itemRenderer.bind(this);
         this.UI_LIST_AWARD.numItems = this._data?.ids.length || 0;
     }
 
+    /**
+     * 列表渲染
+     * @param index 
+     * @param obj 
+     */
     itemRenderer(index:number, obj:fgui.GObject):void{
         const id = this._data?.ids[index] ?? 0
         const num = this._data?.nums[index] ??0
--- assets/scripts/view/props/comp/ComProp.ts
+++ assets/scripts/view/props/comp/ComProp.ts
@@ -4,8 +4,16 @@ import * as fgui from "fairygui-cc";
 import { PropInfo } from "../data/PropConfig";
 import { Prop } from "../../../modules/Prop";
 
+/**
+ * 道具组件类
+ */
+
 @ViewClass()
 export class ComProp extends FGUIComProp { 
+    /**
+     * 显示
+     * @param info 
+     */
     show(info:PropInfo):void{
         const data = Prop.create(info.id);
         this.UI_TXT_NUM.text = `x${info.num ?? 0}`
