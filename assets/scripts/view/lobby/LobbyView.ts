1caffdb31b11ebb47758d287b9f7bcc557b1d730
kakamatong <631603920@qq.com>
2026-01-21 09:35:57 +0800

    refactor(lobby): 提取背景动画逻辑至新组件
    - 从 LobbyView 中移除背景动画相关属性和方法
    - 新增 CompBg 组件，封装背景图移动与重置的逻辑
    - 保持背景节点移动的平滑与复位位置不变
    - 更新相关元数据文件以支持新组件加载
    - 优化模块职责划分，提高代码可维护性

--- assets/scripts/view/lobby/LobbyView.ts
+++ assets/scripts/view/lobby/LobbyView.ts
@@ -30,12 +30,6 @@ import { sys } from 'cc';
 @PackageLoad(['common','props'])
 @ViewClass()
 export class LobbyView extends FGUILobbyView {
-
-    private _node1: fgui.GObject | null = null;
-    private _node2: fgui.GObject | null = null;
-    private _node3: fgui.GObject | null = null;
-    private _node4: fgui.GObject | null = null;
-
     onConstruct(){
         super.onConstruct();
         this.initListeners();
@@ -88,10 +82,6 @@ export class LobbyView extends FGUILobbyView {
      * 初始化UI
      */
     initUI(){ 
-        this._node1 = this.UI_COMP_BG_ACT.UI_COMP_BG_ACT_1
-        this._node2 = this.UI_COMP_BG_ACT.UI_COMP_BG_ACT_2
-        this._node3 = this.UI_COMP_BG_ACT.UI_COMP_BG_ACT_3
-        this._node4 = this.UI_COMP_BG_ACT.UI_COMP_BG_ACT_4
         if (DataCenter.instance.isEnvDev()) {
             this.UI_TXT_ENV.visible = true
         }
@@ -324,48 +314,6 @@ export class LobbyView extends FGUILobbyView {
                 TipsView.showView({content:`游戏服务器连接失败`})
             }
         })
-    }
-
-    /**
-     * 计时器回调
-     * @returns 
-     */
-    onUpdate():void{
-        if (!this._node1 || !this._node2 || !this._node3 || !this._node4) {
-            return
-        }
-        const d = 0.5
-        this._node1.x = this._node1.x - d
-        this._node1.y = this._node1.y - d
-
-        this._node2.x = this._node2.x - d
-        this._node2.y = this._node2.y - d
-
-        this._node3.x = this._node3.x - d
-        this._node3.y = this._node3.y - d
-
-        this._node4.x = this._node4.x - d
-        this._node4.y = this._node4.y - d
-
-        if (this._node1.x < -1530) {
-            this._node1.x = 210
-            this._node1.y = 902
-        }
-
-        if (this._node2.x < -1530) {
-            this._node2.x = 210
-            this._node2.y = 902
-        }
-
-        if (this._node3.x < -1530) {
-            this._node3.x = 210
-            this._node3.y = 902
-        }
-
-        if (this._node4.x < -1530) {
-            this._node4.x = 210
-            this._node4.y = 902
-        }
     }
 
     /**
---
+++ assets/scripts/view/lobby/comp/CompBg.ts
@@ -0,0 +1,66 @@
+import FGUICompBg from "../../../fgui/lobby/FGUICompBg";
+import { ViewClass } from "../../../frameworks/Framework";
+import * as fgui from "fairygui-cc";
+
+@ViewClass()
+export class CompBg extends FGUICompBg {
+    private _node1: fgui.GObject | null = null;
+    private _node2: fgui.GObject | null = null;
+    private _node3: fgui.GObject | null = null;
+    private _node4: fgui.GObject | null = null;
+
+    onConstruct(): void {
+        super.onConstruct()
+        this.initUI()
+    }
+
+    initUI(): void {
+        this._node1 = this.UI_COMP_BG_ACT.UI_COMP_BG_ACT_1
+        this._node2 = this.UI_COMP_BG_ACT.UI_COMP_BG_ACT_2
+        this._node3 = this.UI_COMP_BG_ACT.UI_COMP_BG_ACT_3
+        this._node4 = this.UI_COMP_BG_ACT.UI_COMP_BG_ACT_4
+    }
+
+    /**
+     * 计时器回调
+     * @returns 
+     */
+    onUpdate():void{
+        if (!this._node1 || !this._node2 || !this._node3 || !this._node4) {
+            return
+        }
+        const d = 0.5
+        this._node1.x = this._node1.x - d
+        this._node1.y = this._node1.y - d
+
+        this._node2.x = this._node2.x - d
+        this._node2.y = this._node2.y - d
+
+        this._node3.x = this._node3.x - d
+        this._node3.y = this._node3.y - d
+
+        this._node4.x = this._node4.x - d
+        this._node4.y = this._node4.y - d
+
+        if (this._node1.x < -1530) {
+            this._node1.x = 210
+            this._node1.y = 902
+        }
+
+        if (this._node2.x < -1530) {
+            this._node2.x = 210
+            this._node2.y = 902
+        }
+
+        if (this._node3.x < -1530) {
+            this._node3.x = 210
+            this._node3.y = 902
+        }
+
+        if (this._node4.x < -1530) {
+            this._node4.x = 210
+            this._node4.y = 902
+        }
+    }
+}
+fgui.UIObjectFactory.setExtension(CompBg.URL, CompBg);
